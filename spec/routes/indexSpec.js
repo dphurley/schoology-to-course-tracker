d74ed0f12bdaefcd8e79e8dc64f249e23bb63ae7
D.P. Hurley <hurley.dannyp@gmail.com>
2017-06-13 16:00:16 -0400

    Pulled out Karma. Set up Jasmine to test Express endpoints. Would love to figure out how to deal with potential test-pollution through cache-busting Node when new Server is required.

--- spec/routes/indexSpec.js
+++ spec/routes/indexSpec.js
@@ -1,5 +1,17 @@
+var request = require('supertest');
+var finishTestcase = require('jasmine-supertest');
+
 describe('setting up testing framework', function () {
-  it('runs', function () {
-    expect(true).toBeTruthy();
+
+  beforeEach(function () {
+    server = require(__dirname + '/../../bin/www', {bustCache: true});
   });
+
+  it('responds to /', (done) => {
+    request(server)
+      .get('/')
+      .expect(200)
+      .end(finishTestcase(done));
+  });
+
 });
